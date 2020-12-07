4090c799a26fe855401882c1458add58bc3bb45c
April Darger <april.m.darger@gmail.com>
2020-12-07 16:47:37 -0700

    Working on writing put endpoint another way. Still need to finish delete endpoint.

---
+++ api/accounts/accounts-model.js
@@ -0,0 +1,43 @@
+const db = require('../../data/dbConfig');
+
+module.exports = {
+  getAll,
+  getByID,
+  create,
+  update,
+  remove
+}
+
+function getAll() {
+  // SELECT * FROM accounts;
+  return db('accounts')
+}
+
+function getByID(id) {
+  // SELECT * FROM accounts WHERE id = ...;
+  return db('accounts').where({ id }).first()
+}
+
+function create(account) {
+  // INSERT INTO accounts (name, budget)
+  // VALUES ('foo', 'bar');
+  return db('accounts').insert(account)
+    .then(([id]) => {
+      return db('accounts').where({ id }).first()
+    })
+}
+
+function update(id, account) {
+ // UPDATE accounts SET title = 'foo', contents = 'bar'
+ // WHERE id = 1;
+ return db('accounts').update(account)
+  .then(([id]) => {
+    return db('accounts').where({ id }).first()
+  })
+}
+
+function remove(id) {
+ //DELETE FROM accounts WHERE id = 1;
+ return db('accounts').where({ id }).del()
+}
+
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const db = require("./accounts/accounts-router");
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', db);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
 
 module.exports = server;
